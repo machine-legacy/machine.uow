0aef510103f3087a340a6617603e0cb2bed1d399
Jacob Lewallen <jlewallen@gmail.com>
2008-09-27 23:55:29 -0700

    Adding generic IUnitOfWorkEvents to make future impls prettier.

---
+++ Source/Machine.UoW/GenericUnitOfWorkEvents.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace Machine.UoW
+{
+  public class GenericUnitOfWorkEvents<T> : IUnitOfWorkEvents
+  {
+    #region IUnitOfWorkEvents Members
+    public virtual bool AppliesToObject(object obj)
+    {
+      return typeof(T).IsInstanceOfType(obj);
+    }
+
+    public void AddNew(object obj)
+    {
+      AddNew((T)obj);
+    }
+
+    public void Save(object obj)
+    {
+      Save((T)obj);
+    }
+
+    public void Delete(object obj)
+    {
+      Delete((T)obj);
+    }
+
+    public void Rollback(object obj)
+    {
+      Rollback((T)obj);
+    }
+    #endregion
+
+    public virtual void AddNew(T obj)
+    {
+    }
+
+    public virtual void Save(T obj)
+    {
+    }
+
+    public virtual void Delete(T obj)
+    {
+    }
+
+    public virtual void Rollback(T obj)
+    {
+    }
+  }
+}
