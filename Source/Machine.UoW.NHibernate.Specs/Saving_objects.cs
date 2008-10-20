223026b88a1bbffabff37cc986f2fde6ba24e61e
Jacob Lewallen <jlewallen@gmail.com>
2008-10-20 16:10:14 -0700

    Saving objects (just loading them and changing them)
    Works as expected. NH is doing most of the work for us... Heh.

---
+++ Source/Machine.UoW.NHibernate.Specs/Saving_objects.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+
+using Machine.Specifications;
+using Machine.UoW.NHibernate.Specs.NorthwindModel;
+
+namespace Machine.UoW.NHibernate.Specs
+{
+  [Subject("Saving objects")]
+  public class when_saving_an_object : with_nhibernate_uow
+  {
+    static NorthwindEmployee employee;
+    static long id;
+
+    Establish context = () => id = database.AddSingleEmployee();
+
+    Because of = () =>
+    {
+      using (IUnitOfWork uow = UoW.Start())
+      {
+        employee = uow.Session().Get<NorthwindEmployee>(id);
+        employee.FirstName = "Steve Van";
+        uow.Commit();
+      }
+      using (IUnitOfWork uow = UoW.Start())
+      {
+        employee = uow.Session().Get<NorthwindEmployee>(id);
+      }
+    };
+
+    It should_be_saved = () =>
+      employee.FirstName.ShouldEqual("Steve Van");
+  }
+  
+  [Subject("Saving objects")]
+  public class when_saving_an_object_and_rolling_back : with_nhibernate_uow
+  {
+    static NorthwindEmployee employee;
+    static long id;
+
+    Establish context = () => id = database.AddSingleEmployee();
+
+    Because of = () =>
+    {
+      using (IUnitOfWork uow = UoW.Start())
+      {
+        employee = uow.Session().Get<NorthwindEmployee>(id);
+        employee.FirstName = "Steve Van";
+      }
+      using (IUnitOfWork uow = UoW.Start())
+      {
+        employee = uow.Session().Get<NorthwindEmployee>(id);
+      }
+    };
+
+    It should_not_be_saved = () =>
+      employee.FirstName.ShouldNotEqual("Steve Van");
+  }
+}
