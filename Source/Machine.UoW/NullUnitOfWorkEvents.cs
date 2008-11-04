d0899f6e401e6e195f243f68ccb990f8fa2304bc
Jacob Lewallen <jlewallen@gmail.com>
2008-11-04 13:27:38 -0800

    Adding NullUnitOfWorkEvents that does nothing.

---
+++ Source/Machine.UoW/NullUnitOfWorkEvents.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+namespace Machine.UoW
+{
+  public class NullUnitOfWorkEvents : IUnitOfWorkEvents
+  {
+    public void Start(IUnitOfWork unitOfWork)
+    {
+    }
+
+    public void AddNew(IUnitOfWork unitOfWork, object obj)
+    {
+    }
+
+    public void Save(IUnitOfWork unitOfWork, object obj)
+    {
+    }
+
+    public void Delete(IUnitOfWork unitOfWork, object obj)
+    {
+    }
+
+    public void Rollback(IUnitOfWork unitOfWork, object obj)
+    {
+    }
+
+    public void Rollback(IUnitOfWork unitOfWork)
+    {
+    }
+
+    public void Commit(IUnitOfWork unitOfWork)
+    {
+    }
+  }
+}
