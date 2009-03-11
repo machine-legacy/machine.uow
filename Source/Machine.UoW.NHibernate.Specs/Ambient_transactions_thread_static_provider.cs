d2bd777ecaad7e5d4efcf971db0aa3fa2f4678c8
Jacob Lewallen <jlewallen@gmail.com>
2009-03-11 12:56:29 -0700

    Renamed ambient transaction specifications files.

--- Source/Machine.UoW.NHibernate.Specs/Ambient_transactions_thread_static_provider.cs
+++ Source/Machine.UoW.NHibernate.Specs/Ambient_transactions_thread_static_provider.cs
@@ -5,7 +5,7 @@ using System.Transactions;
 using Machine.Specifications;
 using Machine.UoW.NHibernate.Specs.NorthwindModel;
 
-namespace Machine.UoW.NHibernate.Specs.AmbientTransactions
+namespace Machine.UoW.NHibernate.Specs.AmbientTransactionsAndThreadStaticProvider
 {
   [Subject("Ambient Transactions")]
   public class when_saving_an_object : with_nhibernate_uow
@@ -102,7 +102,7 @@ namespace Machine.UoW.NHibernate.Specs.AmbientTransactions
       }
     };
 
-    It should_not_be_changed_in_database = () =>
+    It should_not_be_changed_in_database_because_connection_automatically_enlists = () =>
       employee.FirstName.ShouldEqual("Steve");
   }
 
@@ -126,6 +126,43 @@ namespace Machine.UoW.NHibernate.Specs.AmbientTransactions
             employee.FirstName = "Steve Van";
             throw new InvalidOperationException();
           }
+        }
+      }
+      catch
+      {
+        using (IUnitOfWork uow = UoW.Start())
+        {
+          employee = uow.Session().Get<NorthwindEmployee>(id);
+          uow.Commit();
+        }
+      }
+    };
+
+    It should_not_be_changed_in_database = () =>
+      employee.FirstName.ShouldEqual("Steve");
+  }
+
+  [Subject("Ambient Transactions")]
+  public class when_saving_an_object_and_uow_is_rolled_back : with_nhibernate_uow
+  {
+    static NorthwindEmployee employee;
+    static long id;
+
+    Establish context = () => id = database.AddSingleEmployee();
+
+    Because of = () =>
+    {
+      try
+      {
+        using (TransactionScope txn = new TransactionScope())
+        {
+          using (IUnitOfWork uow = UoW.Start())
+          {
+            employee = uow.Session().Get<NorthwindEmployee>(id);
+            employee.FirstName = "Steve Van";
+            uow.Rollback();
+          }
+          txn.Complete();
         }
       }
       catch
