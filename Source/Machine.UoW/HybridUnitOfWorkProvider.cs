e574367db821b7583dd3a9bea30cf81a6be9858c
Jacob Lewallen <jlewallen@gmail.com>
2009-03-11 15:26:14 -0700

    Hybrid provider will use the transaction scope provider first.

--- Source/Machine.UoW/HybridUnitOfWorkProvider.cs
+++ Source/Machine.UoW/HybridUnitOfWorkProvider.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Transactions;
 using System.Web;
+
+using Machine.UoW.AmbientTransactions;
 
 namespace Machine.UoW
 {
@@ -8,11 +11,13 @@ namespace Machine.UoW
   {
     readonly HttpContextUnitOfWorkProvider _httpContextUnitOfWorkProvider;
     readonly ThreadStaticUnitOfWorkProvider _threadStaticUnitOfWorkProvider;
+    readonly AmbientTransactionUoWProvider _ambientTransactionUoWProvider;
 
     public HybridUnitOfWorkProvider(IUnitOfWorkFactory unitOfWorkFactory)
     {
       _httpContextUnitOfWorkProvider = new HttpContextUnitOfWorkProvider(unitOfWorkFactory);
       _threadStaticUnitOfWorkProvider = new ThreadStaticUnitOfWorkProvider(unitOfWorkFactory);
+      _ambientTransactionUoWProvider = new AmbientTransactionUoWProvider(unitOfWorkFactory);
     }
 
     public IUnitOfWork Start(IUnitOfWorkSettings[] settings)
@@ -27,6 +32,10 @@ namespace Machine.UoW
 
     private IUnitOfWorkProvider GetUnitOfWorkProviderToUse()
     {
+      if (Transaction.Current != null)
+      {
+        return _ambientTransactionUoWProvider;
+      }
       if (HttpContext.Current != null)
       {
         return _httpContextUnitOfWorkProvider;
