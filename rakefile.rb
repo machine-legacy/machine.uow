b4df0ee3881b146f2e9b1b0472579779594353f5
Jacob Lewallen <jlewallen@gmail.com>
2009-03-12 17:20:13 -0700

    Adding new Machine. Also changing NH specs to AnyCPU, while we are SqlServer.

--- rakefile.rb
+++ rakefile.rb
@@ -1,4 +1,4 @@
 task :specs do
-  sh "Libraries/Machine/Specifications/Machine.Specifications.ConsoleRunner.exe Source/Machine.UoW.Specs/bin/Debug/Machine.UoW.Specs.dll --html Specifications.html"
+  sh "Libraries/Machine/Specifications/Machine.Specifications.ConsoleRunner.exe Build/Debug/Machine.UoW.NHibernate.Specs.dll Build/Debug/Machine.UoW.Specs.dll"
 end
 
