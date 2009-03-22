cae302eaddd5218f23238c8b83ab40f502ca4d1a
Jacob Lewallen <jlewallen@gmail.com>
2009-03-22 15:31:39 -0700

    Better rakefile, specifications, etc...

--- rakefile.rb
+++
@@ -1,4 +0,0 @@
-task :specs do
-  sh "Libraries/Machine/Specifications/Machine.Specifications.ConsoleRunner.exe Build/Debug/Machine.UoW.NHibernate.Specs.dll Build/Debug/Machine.UoW.Specs.dll"
-end
-
