10221bf76b027dc2b433bb64669d1411d72ebd58
Jacob Lewallen <jlewallen@gmail.com>
2009-10-29 11:46:42 -0700

    Using new ContextStorage for NH.

--- Source/Machine.UoW.NHibernate/NH.cs
+++ Source/Machine.UoW.NHibernate/NH.cs
@@ -1,15 +1,18 @@
 using System;
 using System.Collections.Generic;
-using System.Web;
+
+using Machine.UoW.DatabaseContext;
+using Machine.UoW.DatabaseContext.Web;
+
 using NHibernate;
 
 namespace Machine.UoW.NHibernate
 {
   public static class NH
   {
-    static ISessionStorage _sessionStorage = new ThreadStaticAndHttpContextSessionStorage();
+    static IContextStorage<ISession> _sessionStorage = new ThreadStaticAndHttpContextStorage<ISession>();
 
-    public static ISessionStorage Storage
+    public static IContextStorage<ISession> Storage
     {
       get { return _sessionStorage; }
       set { _sessionStorage = value; }
@@ -17,109 +20,13 @@ namespace Machine.UoW.NHibernate
 
     public static ISession Session
     {
-      get { return _sessionStorage.Session; }
-      set { _sessionStorage.Session = value; }
+      get { return _sessionStorage.StoredValue; }
+      set { _sessionStorage.StoredValue = value; }
     }
 
     public static bool HasSession
     {
-      get { return _sessionStorage.HasSession; }
+      get { return _sessionStorage.HasValue; }
     }
-  }
-
-  public interface ISessionStorage
-  {
-    ISession Session
-    {
-      get;
-      set;
-    }
-
-    bool HasSession { get; }
-  }
-
-  public abstract class SessionStorageBase : ISessionStorage
-  {
-    protected abstract ISession InternalSession
-    {
-      get;
-      set;
-    }
-
-    public ISession Session
-    {
-      get
-      {
-        if (InternalSession == null)
-        {
-          throw new NoNHibernateSessionException();
-        }
-        return InternalSession;
-      }
-      set
-      {
-        if (InternalSession != value)
-        {
-          if (InternalSession != null && value != null)
-          {
-            throw new InvalidOperationException("Trying to use another Session when one is already in use!");
-          }
-        }
-        InternalSession = value;
-      }
-    }
-
-    public bool HasSession
-    {
-      get { return InternalSession != null; }
-    }
-  }
-
-  public class HttpContextSessionStorage : SessionStorageBase
-  {
-    protected override ISession InternalSession
-    {
-      get { return (ISession)HttpContext.Current.Items[typeof(HttpContextSessionStorage).FullName]; }
-      set { HttpContext.Current.Items[typeof(HttpContextSessionStorage).FullName] = value; }
-    }
-  }
-
-  public class ThreadStaticSessionStorage : SessionStorageBase
-  {
-    [ThreadStatic]
-    private static ISession _session;
-
-    protected override ISession InternalSession
-    {
-      get { return _session; }
-      set { _session = value; }
-    }
-  }
-
-  public class ThreadStaticAndHttpContextSessionStorage : ISessionStorage
-  {
-    public ISession Session
-    {
-      get { return this.CurrentStorage.Session; }
-      set { this.CurrentStorage.Session = value; }
-    }
-
-    public bool HasSession
-    {
-      get { return this.CurrentStorage.HasSession; }
-    }
-
-    ISessionStorage CurrentStorage
-    {
-      get
-      {
-        if (HttpContext.Current == null)
-          return _threadStaticAndHttpContextSessionStorage;
-        return _httpContextSessionStorage;
-      }
-    }
-
-    readonly ISessionStorage _threadStaticAndHttpContextSessionStorage = new ThreadStaticSessionStorage();
-    readonly ISessionStorage _httpContextSessionStorage = new HttpContextSessionStorage();
   }
 }
